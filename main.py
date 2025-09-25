3f9cb467ae91b9a2f85838b285d6e86e69b5cbc0
ivannamal <imalaschuk@gmail.com>
2025-09-25 13:58:48 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def evaluate(my_list):
+    n = len(my_list)
+    desired_pos = int(((2 * n) / 3) - 1)
+    if n <= 1:
+        return None
+    desired_element = my_list[desired_pos]
+    return desired_element
+
+
+print(evaluate([0, 1, 2, 3, 4]))
